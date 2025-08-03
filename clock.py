7bc26087ef6d4727d8d74254b6c5a4ac1ebc1725
wkneiszel <72527316+wkneiszel@users.noreply.github.com>
2025-08-03 07:43:58 -0500

    Add minutes

--- clock.py
+++ clock.py
@@ -19,9 +19,29 @@ while True:
     hourImages = os.listdir("hours/"+hour)
     hourImagePath = "hours/"+hour+"/"+hourImages[random.randint(0,len(hourImages)-1)]
 
+    colonImages = os.listdir("colon")
+    colonImagePath = "colon/"+colonImages[random.randint(0,len(colonImages)-1)]
+
+    minTenImages = os.listdir("minute-tens/"+minuteTens)
+    minTenImagePath = "minute-tens/"+minuteTens+"/"+minTenImages[random.randint(0,len(minTenImages)-1)]
+
+    minOneImages = os.listdir("minute-ones/"+minuteOnes)
+    minOneImagePath = "minute-ones/"+minuteOnes+"/"+minOneImages[random.randint(0,len(minOneImages)-1)]
+
     img = Image.open(bgImagePath)
+
     hourImage = Image.open(hourImagePath)
     img.paste(hourImage, (0,0), hourImage)
+
+    colonImage = Image.open(colonImagePath)
+    img.paste(colonImage, (64,0), colonImage)
+
+    minTenImage = Image.open(minTenImagePath)
+    img.paste(minTenImage, (96,0), minTenImage)
+
+    minOneImage = Image.open(minOneImagePath)
+    img.paste(minOneImage, (128,0), minOneImage)
+
     img.save("time.png")
 
     #subprocess.run(["sudo", "fbi", "-a", "-T", "1", "time.png"])
